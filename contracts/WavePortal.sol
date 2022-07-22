ed681632a3f92a4ebf53c1c4425b1fb3a2e14504
13JaviG <13javiergc@gmail.com>
2022-07-22 13:24:55 +0200

    first commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract WavePortalNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory name, string memory symbol)
+        ERC721(name, symbol)
+    {}
+}
